cf392deb5800c96660ddbfa0c7e66d0199a1489d
YongjieXue <1018429901@qq.com>
2020-03-17 16:45:53 +0800

    add echart

--- src/layout/LayoutRouter.jsx
+++ src/layout/LayoutRouter.jsx
@@ -3,6 +3,7 @@ import {Switch,HashRouter,Route} from 'react-router-dom'
 import TableContain from "../page/Table";
 import DrawContain from "../page/Draw";
 import ModalContain from '../page/Modal/ModalContain'
+import Echarts from '../page/Echarts'
 const {Component} = React;
 
 class LayoutRouter extends Component {
@@ -17,6 +18,8 @@ class LayoutRouter extends Component {
                     <Route path={'/table'} component={TableContain}></Route>
                     <Route path={'/draw'} component={DrawContain}></Route>
                     <Route path={'/modal'} component={ModalContain}></Route>
+                    <Route path={'/echarts'} component={Echarts}></Route>
+
                     <Route render={()=><div>No thing</div>}></Route>
                 </Switch>
             </HashRouter>
---
+++ src/page/Echarts.jsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import ReactEcharts from 'echarts-for-react'
+const {Component} = React;
+const rs={"code":"success","message":null,"data":[{"projectId":1502085794507232,"componentId":1622535761306208,"componentCode":"18.02.02-P011X008","componentBrand":"科勒","componentType":"智能马桶K-5401T-3-0","componentTitle":"智能马桶","spaceId":2276419022970248,"spaceName":"主卫","customType":3,"count":1,"houseCount":1,"rate":1.0,"price":12678.00},{"projectId":1502085794507232,"componentId":1576317344816768,"componentCode":"02.08.04-P022X002","componentBrand":"欧神诺","componentType":"ELN10880S","componentTitle":"书房-实木地板","spaceId":2276419022970290,"spaceName":"客厅","customType":3,"count":50,"houseCount":1,"rate":1.0,"price":0.00},{"projectId":1502085794507232,"componentId":1516324486107200,"componentCode":"02.08.04-P004X005","componentBrand":"马可波罗","componentType":"未知型号","componentTitle":"衣帽间-镶金瓷砖地面","spaceId":2276419022970298,"spaceName":"衣帽间","customType":3,"count":1,"houseCount":1,"rate":1.0,"price":12000.00},{"projectId":1502085794507232,"componentId":1576348147588736,"componentCode":"02.08.04-P022X007","componentBrand":"欧神诺","componentType":"未知型号-X007","componentTitle":"客餐厅-镶金瓷砖","spaceId":2276419022970290,"spaceName":"客厅","customType":3,"count":1,"houseCount":1,"rate":1.0,"price":0.00},{"projectId":1502085794507232,"componentId":1621062860186208,"componentCode":"24.02.04-P012X009","componentBrand":"三雄极光","componentType":"PAK565212  7W  3000K","componentTitle":"主卧品饰包-轻奢版","spaceId":2276419022970238,"spaceName":"主卧","customType":3,"count":1,"houseCount":1,"rate":1.0,"price":7890.00},{"projectId":1502085794507232,"componentId":1494369996564640,"componentCode":"02.08.02-P005X001","componentBrand":"圣象","componentType":"WHIHB18083105","componentTitle":"主卧实木地板","spaceId":2276419022970238,"spaceName":"主卧","customType":3,"count":1,"houseCount":1,"rate":1.0,"price":12000.00},{"projectId":1502085794507232,"componentId":1621062859792992,"componentCode":"22.06.02-P002X068","componentBrand":"定制","componentType":"次卧3-北次卧背景（上叠）（深）","componentTitle":"主卧家具包-雅致版","spaceId":2276419022970238,"spaceName":"主卧","customType":3,"count":1,"houseCount":1,"rate":1.0,"price":6790.00},{"projectId":1502085794507232,"componentId":1503564931664864,"componentCode":"02.04.10-P001X004","componentBrand":"待定","componentType":"未知型号","componentTitle":"壁纸WP-02","spaceId":2276419022970290,"spaceName":"客厅","customType":3,"count":1,"houseCount":1,"rate":1.0,"price":0.00},{"projectId":1502085794507232,"componentId":1621062859407968,"componentCode":"24.02.02-P012X002","componentBrand":"三雄极光","componentType":"PAK-SXFDNLED-09/Y","componentTitle":"客厅品饰包-轻奢版","spaceId":2276419022970290,"spaceName":"客厅","customType":3,"count":1,"houseCount":1,"rate":1.0,"price":34000.00},{"projectId":1502085794507232,"componentId":1621062859104864,"componentCode":"22.06.02-P002X078","componentBrand":"定制","componentType":"武汉天赋A客厅背景墙","componentTitle":"客厅家具包-雅致版","spaceId":2276419022970290,"spaceName":"客厅","customType":3,"count":1,"houseCount":1,"rate":1.0,"price":2390.00},{"projectId":1502085794507232,"componentId":1622535760978528,"componentCode":"18.06.16-P011X009","componentBrand":"科勒","componentType":"科勒魔镜K-76528T-0","componentTitle":"智能魔镜","spaceId":2276419022970248,"spaceName":"主卫","customType":3,"count":1,"houseCount":1,"rate":1.0,"price":23000.00},{"projectId":1502085794507232,"componentId":1551652759932992,"componentCode":"20.12.02-P001X021","componentBrand":"待定","componentType":"未知型号--X021","componentTitle":"书柜","spaceId":2276419022970290,"spaceName":"客厅","customType":2,"count":3,"houseCount":4,"rate":0.75,"price":7800.00},{"projectId":1502085794507232,"componentId":1551673000906816,"componentCode":"20.10.02-P001X020","componentBrand":"待定","componentType":"未知型号--X020","componentTitle":"主卧衣柜","spaceId":2276419022970298,"spaceName":"衣帽间","customType":2,"count":3,"houseCount":4,"rate":0.75,"price":13000.00},{"projectId":1502085794507232,"componentId":null,"componentCode":null,"componentBrand":null,"componentType":null,"componentTitle":"壁纸(壁纸)","spaceId":null,"spaceName":null,"customType":5,"count":2,"houseCount":3,"rate":0.6666666666666666,"price":0.00},{"projectId":1502085794507232,"componentId":null,"componentCode":null,"componentBrand":null,"componentType":null,"componentTitle":"壁纸套餐(壁纸套餐)","spaceId":null,"spaceName":null,"customType":5,"count":2,"houseCount":3,"rate":0.6666666666666666,"price":0.00},{"projectId":1502085794507232,"componentId":1503564927929312,"componentCode":"06.04.10-P019X002","componentBrand":"TOTO","componentType":"未知型号--X002","componentTitle":"独立浴缸","spaceId":2276419022970298,"spaceName":"衣帽间","customType":2,"count":4,"houseCount":8,"rate":0.5,"price":24000.00},{"projectId":1502085794507232,"componentId":1503606796381152,"componentCode":"02.04.14-P002X006","componentBrand":"定制","componentType":"未知型号--X006","componentTitle":"客厅电视背景墙-升级","spaceId":2276419022970290,"spaceName":"客厅","customType":3,"count":3,"houseCount":8,"rate":0.375,"price":7000.00},{"projectId":1502085794507232,"componentId":1494370000373920,"componentCode":"02.08.06-P001X001","componentBrand":"待定","componentType":"未知型号","componentTitle":"NB_楼板饰面-木地板-WD-02升级","spaceId":2276419022970290,"spaceName":"客厅","customType":3,"count":2,"houseCount":7,"rate":0.2857142857142857,"price":0.00},{"projectId":1502085794507232,"componentId":1503564926151648,"componentCode":"02.08.06-P021X006","componentBrand":"天然大理石","componentType":"未知型号--X006","componentTitle":"大理石地面-ST-03","spaceId":2276419022970248,"spaceName":"主卫","customType":3,"count":1,"houseCount":4,"rate":0.25,"price":0.00},{"projectId":1502085794507232,"componentId":1499342311220896,"componentCode":"02.06.10-P001X002","componentBrand":"待定","componentType":"未知型号","componentTitle":"不锈钢踢脚线-MT-02-主卧","spaceId":2276419022970238,"spaceName":"主卧","customType":3,"count":1,"houseCount":4,"rate":0.25,"price":0.00},{"projectId":1502085794507232,"componentId":1503564929330144,"componentCode":"02.04.10-P001X006","componentBrand":"待定","componentType":"未知型号--X006","componentTitle":"主卧衣帽间浴缸背景墙","spaceId":2276419022970298,"spaceName":"衣帽间","customType":2,"count":2,"houseCount":8,"rate":0.25,"price":18000.00},{"projectId":1502085794507232,"componentId":1519063567085632,"componentCode":"02.04.14-P002X003","componentBrand":"定制","componentType":"冰河时代大理石+好奇WF56301U509-772PC","componentTitle":"卧室背景墙-升级","spaceId":2276419022970238,"spaceName":"主卧","customType":3,"count":1,"houseCount":4,"rate":0.25,"price":0.00},{"projectId":1502085794507232,"componentId":1499342311982752,"componentCode":"02.02.02-P003X002","componentBrand":"立邦","componentType":"未知型号","componentTitle":"天花侧面板+金属嵌条（带灯槽）","spaceId":2276419022970238,"spaceName":"主卧","customType":3,"count":1,"houseCount":7,"rate":0.14285714285714285,"price":0.00},{"projectId":1502085794507232,"componentId":1503564930608096,"componentCode":"02.08.04-P021X005","componentBrand":"天然大理石","componentType":"凡士高灰石材","componentTitle":"石材-ST-05-客厅","spaceId":2276419022970290,"spaceName":"客厅","customType":3,"count":1,"houseCount":7,"rate":0.14285714285714285,"price":0.00},{"projectId":1502085794507232,"componentId":1503564930608096,"componentCode":"02.08.04-P021X005","componentBrand":"天然大理石","componentType":"凡士高灰石材","componentTitle":"石材-ST-05-主卧","spaceId":2276419022970238,"spaceName":"主卧","customType":3,"count":1,"houseCount":7,"rate":0.14285714285714285,"price":0.00},{"projectId":1502085794507232,"componentId":1503564929919968,"componentCode":"20.18.02-P008X002","componentBrand":"金牌","componentType":"吊柜（冷光白）+ 好奇WF19202-772PC","componentTitle":"升级","spaceId":2276419022970248,"spaceName":"主卫","customType":3,"count":1,"houseCount":7,"rate":0.14285714285714285,"price":0.00},{"projectId":1502085794507232,"componentId":1499342310925984,"componentCode":"06.04.08-P019X001","componentBrand":"TOTO","componentType":"三干王TYB3061AA+SHXFE21","componentTitle":"落地龙头","spaceId":2276419022970298,"spaceName":"衣帽间","customType":2,"count":1,"houseCount":7,"rate":0.14285714285714285,"price":0.00}]};
+let data=rs.data.map(item=>{item.value=item.count;return item;});
+console.log('data',data);
+const options={
+    title:{
+        text:"Echart数据"
+    },
+    legend:{
+        top:10,
+        left:'center',
+        data:['gcl']/*图例的数据数组。 data 数组项通常为一个字符串，每一项代表一个系列的 name（如果是饼图，也可以是饼图单个数据的 name*/
+    },
+    grid:{//相当于定义网格的大小的和位置
+      left:100,
+      bottom:"20%",
+      right:100,
+    },
+    tooltip: {//在多处可以设置 全局  grid 或者series中都可以
+        /*'item'
+
+        数据项图形触发，主要在散点图，饼图等无类目轴的图表中使用。
+
+        'axis'
+
+        坐标轴触发，主要在柱状图，折线图等会使用类目轴的图表中使用。
+*/
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross'//显示对应的横纵左边
+        },
+        /*板变量有 {a}, {b}，{c}，{d}，{e}，分别表示系列名，数据名，数据值等。 在 trigger 为 'axis' 的时候，会有多个系列的数据，此时可以通过 {a0}, {a1}, {a2} 这种后面加索引的方式表示系列的索引。 不同图表类型下的 {a}，{b}，{c}，{d} 含义不一样。 其中变量{a}, {b}, {c}, {d}在不同图表类型下代表数据含义为：
+
+折线（区域）图、柱状（条形）图、K线图 : {a}（系列名称），{b}（类目值），{c}（数值）, {d}（无）
+
+散点图（气泡）图 : {a}（系列名称），{b}（数据名称），{c}（数值数组）, {d}（无）
+
+地图 : {a}（系列名称），{b}（区域名称），{c}（合并数值）, {d}（无）
+
+饼图、仪表盘、漏斗图: {a}（系列名称），{b}（数据项名称），{c}（数值）, {d}（百分比）*/
+        formatter: (a, b, c,d,e) => { //a 获取的是一个对象数组
+            let {value,componentBrand,componentType}=a[0].data;
+            return `${value}<br />${componentBrand}<br />${componentType}`
+        },
+    },
+    toolbox:{
+            show:true,
+            feature:{
+                saveAsImage:{
+                    show:true,
+                    backgroundColor:"#fff",
+                    excludeComponents:['toolbox'],//保存时忽略的选项
+                }
+            },
+
+    },
+    xAxis:{
+        type:'category',
+        boundaryGap:true,
+        bottom:"20%",
+        axisTick:{
+           // show:false
+            alignWithLabel:true//与label对其
+            },
+        axisLine:{
+            onZero:true,
+            lineStyle:{
+
+            }
+        },
+        axisLabel:{//不属于刻度 属于axis对应的label字段
+            interval:0,
+            rotate:-30,
+            fontsize:10,
+            formatter:(val,index)=>{
+                return `${index}:${val}`//有val和index两个参数
+            }
+        },
+        splitLine: {
+        //  show:true,//所有轴线
+        },
+
+        data:data.map(item=>item['componentTitle']||'test')//有value 属性的对象也可以
+    },
+    yAxis:{
+        type:'value',//只显示最大最小值呢 //固定单位
+        splitLine:{
+            show:false,
+            interval:2,//显示横轴
+        },
+        interval:2,//配合min和max来确定 刻度间的差值
+        min:0,
+        max:"dataMax",
+    },
+    series:[
+        {   name:"gcl",
+            type:"bar",
+            label:{
+
+            },
+
+            barWidth:20,
+            itemStyle:{
+                //borderWidth:20
+                normal:{
+                    color:"#c00"
+                }
+            },
+            data:data
+        }
+    ]
+}
+console.log('options',options)
+
+class Echarts extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<ReactEcharts option={options}
+         style={{
+            width:800,
+            height:600
+         }
+         }
+        />);
+    }
+}
+
+export default Echarts;
